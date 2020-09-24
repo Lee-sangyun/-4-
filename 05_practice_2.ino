c39abb7e7a8ba4a4720a4dbc35c4e61fc9be6132
Lee-sangyun <66247084+Lee-sangyun@users.noreply.github.com>
2020-09-24 23:22:33 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,10 +1,26 @@
 void setup() {
-  pinMode(7, OUTPUT);
-}
-
-void loop() {
-  digitalWrite(7, HIGH);
-  delay(200);
+  pinMode(7,OUTPUT);
   digitalWrite(7, LOW);
-  delay(200);
+  delay(1000); 
+}
+void loop(){
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  digitalWrite(7, 0);
+  delay(100);
+  digitalWrite(7, 1);
+  delay(100);
+  while(1){}
 }
